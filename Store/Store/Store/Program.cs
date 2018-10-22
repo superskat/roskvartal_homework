119d241d6dd70902336bc06f226e8faff5470c05
skat <0estranged0@gmail.com>
2018-10-22 16:28:37 +0300

    Modification and update. Adding a Store Class

--- Store/Store/Store/Program.cs
+++ Store/Store/Store/Program.cs
@@ -7,73 +7,15 @@ namespace Store
 {
     class Program
     {
-        static string[] ClientDiscount = {
-            "DiscountBonusCard",
-            "DiscountPercentCard",
-            "DiscountAmount"
-        };
-
-        static string[] ClientDiscountDescr = {
-            "Бонусная карта",
-            "Скидочная карта",
-            "Сумма от стоимость"
-        };
 
         static void Main(string[] args)
         {
-            if (ClientDiscount.Length != ClientDiscountDescr.Length)
-            {
-                Console.WriteLine("Количество типов скидок не соответствует количеству описаний");
-                Console.WriteLine("Нажмите Enter для завершения");
-                Console.ReadLine();
-
-                Environment.Exit(0);
-            }
-
-            var product = new Product();
-            var discountType = SelectClientDiscount();
-
-            dynamic currentDiscount = Activator.CreateInstance(Type.GetType("Store.Discount." + discountType));
-
-            if (!currentDiscount.IsDiscountValid())
-            {
-                Console.WriteLine("Скидка настроенна некорректно");
-            }
-            else
-            {
-                if (currentDiscount.IsDiscountAvailable())
-                {
-                    Console.WriteLine($"Стоимость товара {product.GetName()} с учетом скидки составляет {currentDiscount.GetDiscountPrice(product.GetPrice())}");
-                }
-                else
-                {
-                    Console.WriteLine("Скидка недоступна");
-                }
-            }
+            new Store();
 
             Console.WriteLine("Нажмите Enter для завершения");
             Console.ReadLine();
             Environment.Exit(0);
         }
-
-        static string SelectClientDiscount()
-        {
-            Console.WriteLine("Выберите тип скидки:");
-
-            for (var i = 0; i < ClientDiscountDescr.Length; i++)
-            {
-                Console.WriteLine((i+1) + " - "+ ClientDiscountDescr[i]);
-            }
-
-            int.TryParse(Console.ReadLine(), out int result);
-
-            if (result > ClientDiscount.Length || result < 1)
-            {
-                Console.WriteLine("Недопустимое значение, посторите попытку");
-                SelectClientDiscount();
-            }
-
-            return ClientDiscount[(result - 1)];
-        }
+        
     }
 }
---
+++ Store/Store/Store/Store.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store
+{
+    class Store
+    {
+        string[] ClientDiscount = {
+            "DiscountBonusCard",
+            "DiscountPercentCard",
+            "DiscountAmount"
+        };
+
+        string[] ClientDiscountDescr = {
+            "Бонусная карта",
+            "Скидочная карта",
+            "Сумма от стоимость"
+        };
+
+        public Store()
+        {
+            Product product;
+            dynamic discount;
+
+            if (this.NeedCreateNewProduct())
+            {
+                product = this.CreateNewProduct();
+                if (this.NeedCreateNewDiscount())
+                {
+                    discount = this.CreateNewDiscount();
+                    if (this.NeedShowDiscountPrice())
+                    {
+                        var discountPrice = this.GetProductDiscountPrice(product, discount);
+                        if (discountPrice <= 0)
+                        {
+                            discountPrice = product.GetPrice();
+                        }
+
+                        Console.WriteLine($"Стоимость товара {product.GetName()} с учетом скидки составляет {discountPrice}");
+                    }
+                }
+            }
+        }
+
+        protected bool NeedCreateNewProduct()
+        {
+            Console.WriteLine("Создать новый товар?");
+            Console.WriteLine("1 - Создать товар");
+            Console.WriteLine("2 - Не создать товар");
+
+            int.TryParse(Console.ReadLine(), out int result);
+
+            if (result == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        protected Product CreateNewProduct()
+        {
+            return new Product();
+        }
+
+        protected bool NeedCreateNewDiscount()
+        {
+            Console.WriteLine("Создать новую скидку?");
+            Console.WriteLine("1 - Создать скидку");
+            Console.WriteLine("2 - Не создать скидку");
+
+            int.TryParse(Console.ReadLine(), out int result);
+
+            if (result == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        protected dynamic CreateNewDiscount()
+        {
+            if (this.ClientDiscount.Length != this.ClientDiscountDescr.Length)
+            {
+                Console.WriteLine("Количество типов скидок не соответствует количеству описаний");
+                Console.WriteLine("Нажмите Enter для завершения");
+                Console.ReadLine();
+
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Выберите тип скидки:");
+
+            for (var i = 0; i < this.ClientDiscountDescr.Length; i++)
+            {
+                Console.WriteLine((i + 1) + " - " + this.ClientDiscountDescr[i]);
+            }
+
+            int.TryParse(Console.ReadLine(), out int result);
+
+            if (result > this.ClientDiscount.Length || result < 1)
+            {
+                Console.WriteLine("Недопустимое значение, посторите попытку");
+                this.CreateNewDiscount();
+            }
+
+            return Activator.CreateInstance(Type.GetType("Store.Discount." + this.ClientDiscount[(result - 1)]));
+        }
+
+        protected bool NeedShowDiscountPrice()
+        {
+            Console.WriteLine("Показать стоимость товара с учетом скидки?");
+            Console.WriteLine("1 - Показать");
+            Console.WriteLine("2 - Не показывать");
+
+            int.TryParse(Console.ReadLine(), out int result);
+
+            if (result == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public float GetProductDiscountPrice(Product product, dynamic discount)
+        {
+            if (!discount.IsDiscountValid())
+            {
+                Console.WriteLine("Скидка настроенна некорректно");
+            }
+            else
+            {
+                if (discount.IsDiscountAvailable())
+                {
+                    return discount.GetDiscountPrice(product.GetPrice());
+                }
+                else
+                {
+                    Console.WriteLine("Скидка недоступна");
+                }
+            }
+
+            return 0;
+        }
+    }
+}
